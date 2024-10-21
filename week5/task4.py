4792bd58eac17615ff7e55d283f0503b99899db2
Tofiq <tekasimov@edu.hse.ru>
2024-10-21 13:28:49 +0300

    week5 passed

---
+++ week5/task4.py
@@ -0,0 +1,13 @@
+"""https://leetcode.com/problems/number-of-boomerangs/"""
+
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        ans = 0
+        for p1 in points:
+            cnt = Counter()
+            for p2 in points:
+                d = dist(p1, p2)
+                ans += cnt[d]
+                cnt[d] += 1
+        return ans << 1
