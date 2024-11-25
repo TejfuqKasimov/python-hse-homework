8ff59aae6c34b9df7a827534b5d2f9bb2d73f79e
Tofiq <tekasimov@edu.hse.ru>
2024-11-25 14:09:48 +0300

    week6 passed

---
+++ week6/task4.py
@@ -0,0 +1,15 @@
+'''https://leetcode.com/problems/contains-duplicate-iii/description/'''
+
+from sortedcontainers import SortedSet
+class Solution:
+    def containsNearbyAlmostDuplicate(
+        self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        s = SortedSet()
+        for i, v in enumerate(nums):
+            j = s.bisect_left(v - valueDiff)
+            if j < len(s) and s[j] <= v + valueDiff:
+                return True
+            s.add(v)
+            if i >= indexDiff:
+                s.remove(nums[i - indexDiff])
+        return False
