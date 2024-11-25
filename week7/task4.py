22c414cce1fda57f34ce44191098c98959f6b5c6
Tofiq <tekasimov@edu.hse.ru>
2024-11-25 14:18:54 +0300

    week7 passed

---
+++ week7/task4.py
@@ -0,0 +1,7 @@
+"""https://leetcode.com/problems/find-k-closest-elements/description/"""
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        arr.sort(key=lambda v: abs(v - x))
+        return sorted(arr[:k])
---
+++ week7/task5.py
@@ -0,0 +1,12 @@
+"""https://leetcode.com/problems/max-consecutive-ones-iii/description/"""
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l = cnt = 0
+        for x in nums:
+            cnt += x ^ 1
+            if cnt > k:
+                cnt -= nums[l] ^ 1
+                l += 1
+        return len(nums) - l
