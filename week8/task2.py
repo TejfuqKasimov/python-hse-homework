bdc13f7764e8718b4697e545a5b9a6fae1d0498b
TeifukKasimov <teifukkasimov@yandex.ru>
2024-12-21 16:13:05 +0300

    week8 passed

---
+++ week8/task2.py
@@ -0,0 +1,13 @@
+"""https://leetcode.com/problems/subarray-product-less-than-k/description/"""
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        ans, s, j = 0, 1, 0
+        for i, v in enumerate(nums):
+            s *= v
+            while j <= i and s >= k:
+                s //= nums[j]
+                j += 1
+            ans += i - j + 1
+        return ans
